c6f54e688f34b46b3ac222a59bce440fa8ad362f
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-02-03 20:20:55 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,28 @@ const app = express();
 const db = mysql.createConnection({
     host: "localhost",
     user:"root",
-    password:"",
+    password:"123@",
+    database: 'your_database_name',
 });
 // connect to the dabatase 
 db.connect((err)=> {
-   if(err) {
-    throw err
-   }
-   console.log("sucessfull connection");
+    if(err){
+        console.log("Error message:",err);
+        return;
+
+    } else {
+        console.log("Successful connected to the database")
+    }
+   //if(err) {
+    //throw err
+  // }
+   //console.log("sucessfull connection");
     
 });
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// add the create the data using this code
 app.get("/createdb",(req,res) =>{
     let sql = "create Database nodemysql";
     db.query(sql,(err,result) => {
@@ -34,10 +46,30 @@ app.get("/createposttable",(req,res)=> {
         console.log("result");
         res.send("DataBase created");
 
-    })
+    });
+});
+
+//PostData using the post request
+app.post("/api/data",(res,req)=> {
+    // the post of the name and the last time
+    const { user , password} = req.body()
+    if(!user || !password) {
+        res.status(400).json({err:"The request was unsuccessful password or username are incorrect"});
+        return;
+    };
+const query = 'INSERT INTO your_table_user (password, user) VALUES (?, ?)'
+connect.query(query,[user,password],(err,query) => {
+    if(err){
+        res.status(500).json({err:"with the server"})
+        return;
+    }
+    res.json({message:"data is upted",id:res.inertedId})
+
+});
+
 });
 
 // where the server is running in the port 3000;
 app.listen("300",() =>{
     console.log("Server run : port 3000");
-} )
+});
